72ddc36f365806ba9df403dcd01eeaf8c8d7c42b
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-02-02 16:09:04 +0000

    code for whichSchool challenge

---
+++ whichSchool.js
@@ -0,0 +1,13 @@
+const age = 12;
+
+  if(age > 0 && age < 13) {
+  	console.log('Elementry School');
+    return 'Elementry School';
+  } else if (age >= 13 && age <= 18) {
+  	console.log('Secondary School');
+    return 'Secondary School';
+  } else {
+  	console.log('Lighthouse Labs');
+    return 'Lighthouse Labs';
+  }
+
