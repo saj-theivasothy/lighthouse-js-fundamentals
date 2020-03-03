06817ab9f03643ddb2e24fb8f5d3fd435c2788d0
Sayinthan Theivasothy <t.saj1994@gmail.com>
2020-03-03 17:25:14 +0000

    Refactored loopyLighthouse2 code

---
+++ loopyLighthouse2.js
@@ -0,0 +1,17 @@
+function loopyLighthouse(range, multiples, words) {  
+  for (let i = range[0]; i <= range[1]; i++) {
+		let output = "";
+		if (i % multiples[0] === 0) {
+			output += words[0];
+		} 
+		
+		if (i % multiples[1] === 0) {
+			output += words[1];
+		}
+		
+		console.log(output === "" ? i : output);
+	}
+} 
+
+//TEST CODE
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
